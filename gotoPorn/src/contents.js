b9dbe0c60cdce4182d9fb6a49a1d6f4a002303df
hhhryoma <poipoi.rh@gmail.com>
2021-02-03 23:57:08 +0900

    commit

--- gotoPorn/src/contents.js
+++ gotoPorn/src/contents.js
@@ -2,13 +2,14 @@
 window.onload = function(){
   const button = document.querySelector('button');
   console.log(button);
-  if(!button){
+  if(button === null){
+    console.log("はい終わり")
     return;
   }
   button.addEventListener('click', event => {
+    console.log("ボタン押されたよ")
     window.alert("のび太さんのエッチーーー！！");
   });
-  // window.alert("read")
 };
 
 // window.onload = () => {
