7ed600327aeeb805f0557e80586863e6bc0912c4
hhhryoma <poipoi.rh@gmail.com>
2021-02-08 01:41:43 +0900

    設定用ポップアップにスタイル適用

--- gotoPorn/src/popup.js
+++ gotoPorn/src/popup.js
@@ -9,10 +9,12 @@ chrome.storage.sync.get("url", (data) => {
     url.value = data.url;
 });
 
+// URL上書きの確率設定イベント
 rate.onchange = (element) => {
     chrome.storage.sync.set({"rate": element.target.value});
 }
 
+// 上書くURLの設定用イベント
 url.onchange = (element) => {
     chrome.storage.sync.set({"url": element.target.value});
 }
