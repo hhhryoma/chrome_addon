17818d929667ae64f456fbf3e0079836ea86cdca
hhhryoma <poipoi.rh@gmail.com>
2021-02-07 23:59:24 +0900

    URLを外部から取得するよう変更

--- gotoPorn/src/contents.js
+++ gotoPorn/src/contents.js
@@ -1,47 +1,32 @@
-const getStorageData = (key) => {
-  return new Promise(resolve => {
-    resolve(chrome.storage.sync.get(key, (result) => {
-      return result.key;
-    }))
+const getData = (key) => {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get(key, (result) => {
+      resolve(result[key])
+    })
   })
 }
 
-const asyncGetStorageData = async (key) => {
-  const ret = await getStorageData(key);
-  return ret;
-}
-
-window.onload = function(){
-  let rate = 0;
-  asyncGetStorageData("rate").then(result => {
-    console.log(result)
-  })
-  // const isGoto = chrome.storage.sync.get(["rate"], (result) => {
-  //   const rnd = Math.floor(Math.random()*100)
-  //   console.log(`rate: ${result.rate}  rand: ${rnd}`);
-  //   return rnd > result.rate
-  // })
-  // console.log(isGoto)
-  // if(!isGoto){
-  //   return;
-  // }
+window.onload = async function(){
+  const rate = await getData("rate");
   console.log(`rate: ${rate}`);
   if(Math.floor(Math.random()*100) > rate) {
     return;
   }
   
-  let a_refs = document.querySelectorAll('a');
-  let url = "https://google.com"
+  let aRefs = document.querySelectorAll('a');
+  const url = await getData("url");
 
-
-  console.log(a_refs);
-  if(a_refs === null){
-    console.log("はい終わり")
+  if(aRefs === null){
+    console.log("はい終わり");
     return;
   }
-  for(const a_ref of a_refs){
+  for(const a_ref of aRefs){
     a_ref.addEventListener('click', event => {
       a_ref.setAttribute("href", url)
     });
   }
+  history.pushState(null, null, location.href);
+  window.addEventListener("popstate", (element) => {
+    history.go(1)
+  })
 };
--- gotoPorn/src/popup.js
+++ gotoPorn/src/popup.js
@@ -1,10 +1,18 @@
 let rate = document.getElementById("rate");
+let url = document.getElementById("url");
 
 chrome.storage.sync.get("rate", (data) => {
     rate.value = data.rate;
 });
 
+chrome.storage.sync.get("url", (data) => {
+    url.value = data.url;
+});
+
 rate.onchange = (element) => {
-    window.alert("hello world");
-    chrome.storage.sync.set({"rate": element.target.value})
+    chrome.storage.sync.set({"rate": element.target.value});
+}
+
+url.onchange = (element) => {
+    chrome.storage.sync.set({"url": element.target.value});
 }
