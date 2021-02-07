c248e15525bfaf922906cdb1610cd6d26a7d3b97
hhhryoma <poipoi.rh@gmail.com>
2021-02-08 01:41:14 +0900

    上書きしたサイトへアクセスした場合に戻るボタン無効化

--- gotoPorn/src/contents.js
+++ gotoPorn/src/contents.js
@@ -1,3 +1,4 @@
+// ストレージから引数のキーに対応する値を取得する
 const getData = (key) => {
   return new Promise((resolve) => {
     chrome.storage.sync.get(key, (result) => {
@@ -8,25 +9,34 @@ const getData = (key) => {
 
 window.onload = async function(){
   const rate = await getData("rate");
-  console.log(`rate: ${rate}`);
+  const url = await getData("url");
+  // URLが指定されていない場合は終了
+  if(url === ""){
+    return;
+  }
+  // 上書き後のURLアクセス時には戻るボタン無効化
+  if(document.URL.match(url)) {
+    history.pushState(null, null, location.href);
+    window.addEventListener("popstate", (element) => {
+      history.go(1);
+    })
+    return;
+  }
+  // URLを上書きするか判定
   if(Math.floor(Math.random()*100) > rate) {
     return;
   }
   
   let aRefs = document.querySelectorAll('a');
-  const url = await getData("url");
 
   if(aRefs === null){
     console.log("はい終わり");
     return;
   }
+  // ページのaタグの遷移先を上書きしていく
   for(const a_ref of aRefs){
     a_ref.addEventListener('click', event => {
       a_ref.setAttribute("href", url)
     });
   }
-  history.pushState(null, null, location.href);
-  window.addEventListener("popstate", (element) => {
-    history.go(1)
-  })
 };
